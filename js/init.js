ac3bbc97cd462075b75f020d6c4044496901f625
Sebastian Walter <sebwalter@gmx.net>
2020-08-11 08:11:10 +0200

    new page

--- js/init.js
+++
@@ -1,6 +0,0 @@
-(function($) {
-
-    //$(".button-collapse").sideNav();
-    // ReSharper disable once PossiblyUnassignedProperty
-    $('.parallax').parallax();
-})(jQuery); // end of jQuery name space
